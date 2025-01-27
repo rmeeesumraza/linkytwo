a12acbaf53ec4c92aa4aa6ac9a9faba958031b5e
rmeeesumraza <github@snobay.com>
2025-01-27 18:06:27 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.alnasser.pk/collections/men-footwear/products/men-formal-shoes-26";
-    const blackPageURL = "https://qiakzlwoapwrda.myfunnelish.com/chelsea-boden-boots-1737635084899211-1737635118902954-1737844416815572";
+    const whitePageURL = "https://blackcamel.pk/products/black-camel-gripper-sports-shoes";
+    const blackPageURL = "https://qiakzlwoapwrda.myfunnelish.com/chelsea-boden-boots-1737635084899211-1737635118902954-1737635126522108";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
