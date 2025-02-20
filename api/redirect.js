54492e988cd86fa09213714ac6b7de64bc78b696
rmeeesumraza <github@snobay.com>
2025-02-20 22:51:00 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://funkyntrendyy.myshopify.com/products/copy-of-funky-n-trendy-hand-painted-water-resistant-winnie-the-pooh-theme-casual-shoes";
-    const blackPageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558";
+    const whitePageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/jj-dd-spr-1739907639969671";
+    const blackPageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/jj-dd-spr-1739907639969671";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
